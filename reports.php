a2ca19b5d9fd49bb5ac1bcda496471ed4efb1f00
Michael W. Delaney <michael.delaney@gmail.com>
2016-09-08 16:05:18 -0400

    Update reports.php

--- reports.php
+++ reports.php
@@ -52,7 +52,7 @@ function Reports_post_type() {
 		'show_in_admin_bar'     => true,
 		'show_in_nav_menus'     => true,
 		'can_export'            => true,
-		'has_archive'           => true,		
+		'has_archive'           => false,		
 		'exclude_from_search'   => true,
 		'publicly_queryable'    => true,
 		'capability_type'       => 'page',
